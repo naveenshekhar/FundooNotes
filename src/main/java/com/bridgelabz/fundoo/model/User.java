80e3f9275de23f8294cf1ee12cb04690d3f8461e
naveenshekhar <naveen.shekhar624@gmail.com>
2020-01-08 14:25:34 +0530

    First commit

---
+++ src/main/java/com/bridgelabz/fundoo/model/User.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.fundoo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+}
