f918cd9cf2f21834f9f9c52b76e8ac0ed0ccf194
naveenshekhar <naveen.shekhar624@gmail.com>
2020-01-08 19:22:14 +0530

    insert query updated in repository

--- src/main/java/com/bridgelabz/fundoo/repository/UserRepository.java
+++ src/main/java/com/bridgelabz/fundoo/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.fundoo.repository;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -7,13 +9,16 @@ import org.springframework.data.jpa.repository.Query;
 import com.bridgelabz.fundoo.model.User;
 
 
-public interface UserRepository extends JpaRepository<com.bridgelabz.fundoo.model.User, Long>{
+public interface UserRepository extends JpaRepository<User, Long>{
 	
 	@Query(value = "select * from user where email=?", nativeQuery = true)
 	User checkByEmail(String email);
+	
 	@Modifying
-	@Query("insert into FundooNotes.user (firstName,lastName,phoneNumber,email,isPhoneVerified,isEmailVerified,password) values (?,?,?,?,?,?,?)")
-	public void insertData(String firstName, String lastName, int phoneNumber, boolean isPhoneVerified,
+	@Transactional
+	@Query(value="insert into FundooNotes.user (firstName,lastName,phoneNumber,email,isPhoneVerified,isEmailVerified,password) "
+			+ "values (?,?,?,?,?,?,?)",nativeQuery=true)
+	public void insertData(String firstName, String lastName, int phoneNumber,String email, boolean isPhoneVerified,
 			boolean isEmailVerified, String password);
 
 }
