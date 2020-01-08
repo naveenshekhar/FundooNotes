407f184777eecabb24a3b0b3e0e3c101aae0a35d
naveenshekhar <naveen.shekhar624@gmail.com>
2020-01-08 16:34:30 +0530

    package structure updated model class for new user (registration) created

--- src/main/java/com/bridgelabz/fundoo/aop/jsbd.java
+++
@@ -1,9 +0,0 @@
-package com.bridgelabz.fundoo.aop;
-
-public class jsbd 
-{
-	public static void main(String[] args) {
-		System.out.println("hello");
-	}
-
-}
--- src/main/java/com/bridgelabz/fundoo/model/User.java
+++ src/main/java/com/bridgelabz/fundoo/model/User.java
@@ -1,15 +1,49 @@
 package com.bridgelabz.fundoo.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "user")
-@NoArgsConstructor
-@AllArgsConstructor
+@Setter
+@Getter
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(name = "firstName", nullable = false)
+	@NotEmpty(message = "Enter first name first")
+	private String firstName;
+
+	@Column(name = "lastName", nullable = true)
+	private String lastName;
+	
+	
+	@Column(name = "phoneNumber", nullable = false,unique = true)
+	@NotEmpty(message = "enter phone number")
+	private int phoneNumber;
+	
+	@Column(name = "email", nullable = false, unique = true)
+	@NotEmpty(message = "Email cannot be empty..!!")
+	private String email;
+	
+	@Column(name = "isPhoneVerified", nullable = true)
+	private boolean isPhoneVerified;
+	
+	@Column(name = "isEmailVerified", nullable = false)
+	private boolean isEmailVerified;
+	
+	@Column(name="password", nullable = false, unique = true)
+	@NotEmpty(message = "password cannot be empty..!!")
+	private String password;
 }
